028e0ef842b839344529b6a7e2120688f77db8ed
brayokenya <kiirubrian21@gmail.com>
2024-05-15 13:19:09 +0100

    Chapter 11: Pagiantion

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -17,6 +17,7 @@ const Search: React.FC<SearchProps> = ({ placeholder = "Search..." }) => {
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page','1')
     if (term) {
       params.set('query', term);
     } else {
