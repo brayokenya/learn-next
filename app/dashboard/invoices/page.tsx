2231409eae0c1b70af19af8859dac4be3dcd0b50
brayokenya <kiirubrian21@gmail.com>
2024-05-15 10:39:57 +0100

    fix placeholder error

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,7 +6,6 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 
-
 export default async function Page({
   searchParams,
 }: {
@@ -17,7 +16,7 @@ export default async function Page({
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
- 
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
